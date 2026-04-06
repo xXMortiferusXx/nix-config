a6b37c830242397cea6fee51a8f25bc985f99c61
xXMortiferusXx <backilein@gmail.com>
2026-04-06 22:29:13 +0200

    Update: 2026-04-06 22:29

--- modules/alias.nix
+++ modules/alias.nix
@@ -6,6 +6,7 @@
     nix-switch  = "sudo nixos-rebuild switch --flake /etc/nixos#Mortiferus-PC";
     nix-update  = "pushd /etc/nixos && nix flake update && sudo nixos-rebuild switch --flake .#Mortiferus-PC && popd";
     nix-clean   = "sudo nix-collect-garbage -d && sudo nixos-rebuild switch --flake /etc/nixos#Mortiferus-PC";
+    conf-sync = "cd /etc/nixos && git add . && git commit -m \"Update: $(date +'%Y-%m-%d %H:%M')\" && git push origin main";
     conf = "cd /etc/nixos";    
 
     # Tools & Gaming
