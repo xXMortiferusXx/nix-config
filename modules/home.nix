4e15375b4a4836af4a044b44fb3439a9ed7ae9bd
xXMortiferusXx <backilein@gmail.com>
2026-04-08 09:23:04 +0200

    Update: 2026-04-08 09:23

--- modules/home.nix
+++ modules/home.nix
@@ -1,33 +1,30 @@
 { config, pkgs, ... }:
 
 {
-  # Home-Manager aktivieren
-  programs.home-manager.enable = true;
+  # Dieses Modul gehört zu Home‑Manager
+  home-manager.users.mortiferus = {
 
-  # XDG-Config-Ordner verlinken
-  xdg.configFile = {
-   "niri".source = ../home/mortiferus/config/niri;
-   "noctalia".source = ../home/mortiferus/config/noctalia;
-   "yazi".source = ../home/mortiferus/config/yazi;
-   "zathura".source = ../home/mortiferus/config/zathura;
-   "gtk-3.0".source = ../home/mortiferus/config/gtk-3.0;
-   "gtk-4.0".source = ../home/mortiferus/config/gtk-4.0;
-   "qt5ct".source = ../home/mortiferus/config/qt5ct;
-   "qt6ct".source = ../home/mortiferus/config/qt6ct;
+    # Home‑Manager aktivieren
+    programs.home-manager.enable = true;
+
+    # XDG-Config-Ordner verlinken
+    xdg.configFile = {
+      "niri".source = ../home/mortiferus/config/niri;
+      "noctalia".source = ../home/mortiferus/config/noctalia;
+      "yazi".source = ../home/mortiferus/config/yazi;
+      "zathura".source = ../home/mortiferus/config/zathura;
+      "gtk-3.0".source = ../home/mortiferus/config/gtk-3.0;
+      "gtk-4.0".source = ../home/mortiferus/config/gtk-4.0;
+      "qt5ct".source = ../home/mortiferus/config/qt5ct;
+      "qt6ct".source = ../home/mortiferus/config/qt6ct;
+    };
+
+    # User-Informationen
+    home.username = "mortiferus";
+    home.homeDirectory = "/home/mortiferus";
+
+    # Home-Manager Version
+    home.stateVersion = "25.11";
   };
-
-  # Falls du später User-spezifische Pakete willst:
-  home.packages = with pkgs; [
-    # Beispiel:
-    # yazi
-    # zathura
-  ];
-
-  # User-Informationen
-  home.username = "mortiferus";
-  home.homeDirectory = "/home/mortiferus";
-
-  # Home-Manager Version (Pflicht)
-  home.stateVersion = "25.11";
 }
 
