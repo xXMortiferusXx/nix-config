56a20eea12c7778a12489a783affeef6ce50a5dd
xXMortiferusXx <backilein@gmail.com>
2026-04-08 10:43:24 +0200

    Update: 2026-04-08 10:43

--- modules/home.nix
+++ modules/home.nix
@@ -1,23 +1,29 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  # Dieses Modul gehört zu Home‑Manager
-  home-manager.users.mortiferus = {
+  # Home-Manager Konfiguration für Mortiferus
+  home-manager.users.mortiferus = { config, ... }: {
 
-    # Home‑Manager aktivieren
+    # Home-Manager aktivieren
     programs.home-manager.enable = true;
 
-    # XDG-Config-Ordner verlinken
+    # XDG-Config-Ordner verlinken (Live-Editierbar & im Git gesichert)
     xdg.configFile = {
-      #"niri".source = ../home/mortiferus/config/niri;
+      # 1. Fenstermanager & Regeln
+      "niri".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/mortiferus/config/niri";
+      
+      # 2. Design-System & UI-Elemente
+      "noctalia".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/mortiferus/config/noctalia";
+      
+      # 3. Gaming-Overlay (FPS, Sensoren, CPU/GPU Last)
+      "MangoHud".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/mortiferus/config/MangoHud";
     };
 
     # User-Informationen
     home.username = "mortiferus";
     home.homeDirectory = "/home/mortiferus";
 
-    # Home-Manager Version
+    # Versionierung (Beibehalten)
     home.stateVersion = "25.11";
   };
 }
-
