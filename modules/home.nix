9256aa0bfc6b786dc6c08d03c4623dd00e442cde
xXMortiferusXx <backilein@gmail.com>
2026-04-08 10:09:45 +0200

    Update: 2026-04-08 10:09

--- modules/home.nix
+++ modules/home.nix
@@ -9,14 +9,7 @@
 
     # XDG-Config-Ordner verlinken
     xdg.configFile = {
-      "niri".source = ../home/mortiferus/config/niri;
-      "noctalia".source = ../home/mortiferus/config/noctalia;
-      "yazi".source = ../home/mortiferus/config/yazi;
-      "zathura".source = ../home/mortiferus/config/zathura;
-      "gtk-3.0".source = ../home/mortiferus/config/gtk-3.0;
-      "gtk-4.0".source = ../home/mortiferus/config/gtk-4.0;
-      "qt5ct".source = ../home/mortiferus/config/qt5ct;
-      "qt6ct".source = ../home/mortiferus/config/qt6ct;
+      #"niri".source = ../home/mortiferus/config/niri;
     };
 
     # User-Informationen
