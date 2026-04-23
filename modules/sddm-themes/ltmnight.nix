fdf85bc8784c730bf9106350ca8ae37b3f921832
Steffen Scholz <backilein@gmail.com>
2026-04-23 03:02:52 +0200

    feat: füge SDDM theme.conf mit Cursor-Konfiguration hinzu

--- modules/sddm-themes/ltmnight.nix
+++ modules/sddm-themes/ltmnight.nix
@@ -1,35 +1,44 @@
-{ pkgs }:
-
-pkgs.stdenv.mkDerivation {
-  pname = "sddm-ltmnight-theme";
-  version = "1.2.4";
-  
-  src = pkgs.fetchFromGitHub {
-    owner = "xXMortiferusXx";
-    repo = "ltmnight-sddm-theme";
-    rev = "v1.2.4";
-    sha256 = "sha256-QyCTVclRDqUtYJJwe3Gphrh9BgxMpOl+svEQCaNR+Iw=";
-  };
-
-  nativeBuildInputs = with pkgs; [
-    qt6.qttools
-    qt6.wrapQtAppsHook
-  ];
-
-  dontWrapQtApps = true;
-  
-  installPhase = ''
-    mkdir -p $out/share/sddm/themes/ltmnight
-    cp -aR . $out/share/sddm/themes/ltmnight/
-    
-    # Erstelle Konfigurationsdatei für animierte Shader
-    cat > $out/share/sddm/themes/ltmnight/Themes/hyprltm.conf.user << EOF
-[General]
-Background="ltmnight"
-PartialBlur="true"
-FormPosition="center"
-HideVirtualKeyboard="false"
-VirtualKeyboardAutoShow="false"
-EOF
-  '';
-}
+{ pkgs }:                                                                                             
+                                                                                                      
+pkgs.stdenv.mkDerivation {                                                                            
+  pname = "sddm-ltmnight-theme";                                                                      
+  version = "1.2.4";                                                                                  
+                                                                                                      
+  src = pkgs.fetchFromGitHub {                                                                        
+    owner = "xXMortiferusXx";                                                                         
+    repo = "ltmnight-sddm-theme";                                                                     
+    rev = "v1.2.4";                                                                                   
+    sha256 = "sha256-QyCTVclRDqUtYJJwe3Gphrh9BgxMpOl+svEQCaNR+Iw=";                                   
+  };                                                                                                  
+                                                                                                      
+  nativeBuildInputs = with pkgs; [                                                                    
+    qt6.qttools                                                                                       
+    qt6.wrapQtAppsHook                                                                                
+  ];                                                                                                  
+                                                                                                      
+  dontWrapQtApps = true;                                                                              
+                                                                                                      
+  installPhase = ''                                                                                   
+    mkdir -p $out/share/sddm/themes/ltmnight                                                          
+    cp -aR . $out/share/sddm/themes/ltmnight/                                                         
+                                                                                                      
+    # Offizielle SDDM theme.conf mit expliziter Cursor-Definition                                     
+    cat > $out/share/sddm/themes/ltmnight/theme.conf << EOF                                           
+[SddmGreeterTheme]                                                                                    
+Name=LTMNight                                                                                         
+CursorTheme=Bibata-Modern-Classic                                                                     
+CursorSize=24                                                                                         
+EOF                                                                                                   
+                                                                                                      
+    # Deine bestehende Theme-Konfiguration                                                            
+    cat > $out/share/sddm/themes/ltmnight/Themes/hyprltm.conf.user << EOF                             
+[General]                                                                                             
+Background="ltmnight"                                                                                 
+PartialBlur="true"                                                                                    
+FormPosition="center"                                                                                 
+HideVirtualKeyboard="false"                                                                           
+VirtualKeyboardAutoShow="false"                                                                       
+EOF                                                                                                   
+  '';                                                                                                 
+}                                                                                                     
+                                                         
