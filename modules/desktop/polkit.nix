ace5bcf53ed8a83ae68af4eacc48f36220ecb56d
Steffen Scholz <backilein@gmail.com>
2026-06-26 16:10:45 +0200

    Update: 2026-06-26 16:10

--- modules/desktop/polkit.nix
+++ modules/desktop/polkit.nix
@@ -11,5 +11,24 @@
         return polkit.Result.YES;
       }
     });
+
+    polkit.addRule(function (action, subject) {
+      var network_actions = [
+        "org.freedesktop.NetworkManager.enable-disable-network",
+        "org.freedesktop.NetworkManager.enable-disable-wifi",
+        "org.freedesktop.NetworkManager.enable-disable-wwan",
+        "org.freedesktop.NetworkManager.network-control",
+        "org.freedesktop.NetworkManager.settings.modify.own",
+        "org.freedesktop.NetworkManager.settings.modify.system",
+        "org.freedesktop.NetworkManager.wifi.scan",
+        "org.freedesktop.NetworkManager.wifi.share.open",
+        "org.freedesktop.NetworkManager.wifi.share.protected"
+      ];
+      if (network_actions.indexOf(action.id) >= 0 &&
+          subject.isInGroup("wheel"))
+      {
+        return polkit.Result.YES;
+      }
+    });
   '';
 }
